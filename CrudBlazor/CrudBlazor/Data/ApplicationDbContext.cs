0f5fa92e1782184669dde9d0b406a9187367e840
GERARDOALDERETE\ge_ca <ge_call@hotmail.com>
2025-07-25 09:38:54 -0600

    feat/Configuracion de conexion a BD y Modelo Libro

---
+++ CrudBlazor/CrudBlazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudBlazor.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudBlazor.Data;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    {
+
+    }
+
+    //Poner aqui los modelos
+    DbSet<Libro> Libros { get; set; }
+}
---
+++ CrudBlazor/CrudBlazor/Modelos/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudBlazor.Modelos;
+
+public class Libro
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Titulo { get; set; } = string.Empty;
+    [Required]
+    public string Descripcion { get; set; } = string.Empty;
+    [Required]
+    public string Autor { get; set; } = string.Empty;
+    [Required]
+    public int Paginas { get; set; }
+    [Required]
+    public int Precio { get; set; }
+    [Required]
+    public DateTime FechaCreacion { get; set; } 
+}
